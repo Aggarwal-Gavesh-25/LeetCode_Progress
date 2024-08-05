ac8981510c529b3015b91b2bb97af20491acbade
Gavesh Aggarwal <118240223+Aggarwal-Gavesh-25@users.noreply.github.com>
2024-08-05 17:05:47 +0530

    Time: 3 ms (25.65%), Space: 8.4 MB (19.71%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int num=x;
+        
+        int result = 0;
+        
+        while(x != 0){
+            int a = x%10;
+            
+            // Check for overflow/underflow before updating result
+            if (result > INT_MAX / 10) return 0;
+            if (result < INT_MIN / 10) return 0;
+            
+            result = result*10 + a;
+            
+            x = x/10;
+        }
+
+        
+        return result;
+    }
+};
