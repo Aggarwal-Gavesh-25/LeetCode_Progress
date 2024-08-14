01d1f73748b93bfa321bde2fc612d53665417e19
Gavesh Aggarwal <118240223+Aggarwal-Gavesh-25@users.noreply.github.com>
2024-08-14 22:10:44 +0530

    Time: 23 ms (34.99%), Space: 27.5 MB (69.71%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -5,6 +5,7 @@ public:
         int n = nums.size();
         k = k % n;
         
+        // if rotate to the left, then reverse(nums.begin(), nums.begin()+n); comes at 3rd step
         reverse(nums.begin(), nums.begin()+n);
         reverse(nums.begin(), nums.begin()+k);
         reverse(nums.begin()+k, nums.begin()+n);
