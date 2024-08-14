fcfb3a183c8666cd366ca1994381490848a648b6
Gavesh Aggarwal <118240223+Aggarwal-Gavesh-25@users.noreply.github.com>
2024-08-14 22:19:42 +0530

    Time: 16 ms (53.48%), Space: 23.1 MB (5.48%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        vector<int> arr;
+        int count=0;
+        
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] != 0){
+                arr.push_back(nums[i]);
+            }
+            else{
+                count++;
+            }
+        }
+        while(count>0){
+            arr.push_back(0);
+            count--;
+        }
+        nums=arr;
+    }
+};
