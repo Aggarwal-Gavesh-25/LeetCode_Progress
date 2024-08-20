823b05f18d1e2735f5a83be63d866d16ff622da3
Gavesh Aggarwal <118240223+Aggarwal-Gavesh-25@users.noreply.github.com>
2024-08-20 21:48:11 +0530

    Time: 313 ms (5.09%), Space: 20.7 MB (60.81%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int max=*max_element(nums.begin(),nums.end());
-        
-        vector<int> varr(max+1);
-        
+        int max = *max_element(nums.begin(),nums.end());
         int i=0;
-        for(i=0; i<nums.size(); i++){
-            varr[nums[i]]=1;
-        }
         
-        for(i=0; i<varr.size(); i++){
-            if(varr[i]==0) break;
+        for(i=0; i<=max; i++){
+            bool found=false;
+            for(auto it:nums){
+                if(it==i) {
+                    found=true;
+                    break;
+                }
+            }
+            if(found == false){
+            break;
+            }
         }
-        
         return i;
-                    
     }
 };
