638b825d0e047954ec401fe02ac8204f905256b4
Gavesh Aggarwal <118240223+Aggarwal-Gavesh-25@users.noreply.github.com>
2024-08-25 00:14:48 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (52.90%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
 bool rotateString(string s, string goal) {
+    // T= n^2
+    // S= n
+    
     if (s.size() != goal.size()) {
         return false;
     }
 
-    string doubled = s + s;
-    if(doubled.find(goal) != string::npos) return true;
+    string doubled = s + s;         // n time & n space
+    if(doubled.find(goal) != string::npos) return true;     // n^2 time
         
     return false;
 }
