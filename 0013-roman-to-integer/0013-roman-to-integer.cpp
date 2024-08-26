f4cc4b128905abbe89342e543b3fc28b3365d743
Gavesh Aggarwal <118240223+Aggarwal-Gavesh-25@users.noreply.github.com>
2024-08-26 15:16:35 +0530

    Time: 10 ms (58.93%), Space: 14.1 MB (27.39%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int romanToInt(string s) {
+        // T= n
+        // S= 1
         map<char,int> mpp;
         mpp['I']=1;
         mpp['V']=5;
@@ -13,30 +15,8 @@ public:
         int result=0;
         
         for(int it=0; it<s.size(); it++){
-            if(s[it]=='I' && s[it+1]=='V'){ 
-                result+=4;
-                it++;
-            }
-            else if(s[it]=='I' && s[it+1]=='X'){ 
-                result+=9;
-                it++;
-            }
-
-            else if(s[it]=='X' && s[it+1]=='L'){
-                result+=40;
-                it++;
-            }
-            else if(s[it]=='X' && s[it+1]=='C'){
-                result+=90;
-                it++;
-            }
-            else if(s[it]=='C' && s[it+1]=='D'){
-                result+=400;
-                it++;
-            }
-            else if(s[it]=='C' && s[it+1]=='M'){
-                result+=900;
-                it++;
+            if(it+1<s.size() && mpp[s[it]]<mpp[s[it+1]]){
+                result -= mpp[s[it]];
             }
             else{
                 result += mpp[s[it]];
