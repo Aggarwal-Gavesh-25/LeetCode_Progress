3e03b35848cd23a38958d730b4daf02f5a8967f2
Gavesh Aggarwal <118240223+Aggarwal-Gavesh-25@users.noreply.github.com>
2024-08-26 14:03:19 +0530

    Time: 5 ms (81.96%), Space: 8.9 MB (51.35%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        
+        vector<int> hash(26);
+        
+        for(auto it:s){
+            hash[it - 'a']++;
+        }
+        for(auto it:t){
+            hash[it - 'a']--;
+        }
+        
+        int maxi=*max_element(hash.begin(),hash.end());
+        if(maxi != 0) return false;
+        
+        return true;
+    }
+};
